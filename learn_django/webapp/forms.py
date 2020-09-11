d2e427dfce3245316662a577251110e3275c1570
MuazamHusnain <muazamhusnain@A006-00309.local>
2020-09-11 19:01:04 +0500

    instead of looping added fields manually in template, added all fields in forms and other suggested changes

--- learn_django/webapp/forms.py
+++ learn_django/webapp/forms.py
@@ -5,8 +5,4 @@ from .models import Customer
 class CustomerModelForm(forms.ModelForm):
     class Meta:
         model = Customer
-        fields = ('name', 'number', 'email', 'company', 'address')
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-
+        fields = '__all__'
