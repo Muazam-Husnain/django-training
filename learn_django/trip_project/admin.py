027f4628c9584269e650f012d2d1a33042435738
MuazamHusnain <muazamhusnain@A006-00309.local>
2020-10-01 15:40:41 +0500

    added necessary fields to models about trip info

--- learn_django/trip_project/admin.py
+++ learn_django/trip_project/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Trip
-
+from .models import Trip, Itenrary
 admin.site.register(Trip)
+admin.site.register(Itenrary)
 
 # Register your models here.
--- learn_django/trip_project/models.py
+++ learn_django/trip_project/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 
 class Trip(models.Model):
@@ -6,3 +7,21 @@ class Trip(models.Model):
     start_date = models.DateField()
     end_date = models.DateField()
     price = models.IntegerField()
+    created_date = models.DateField(auto_now_add=True)
+    added_by = models.ForeignKey(settings.AUTH_USER_MODEL,
+                                 null=True, blank=True, on_delete=models.SET_NULL)
+    host_name = models.CharField(max_length=100, null=True)
+    host_number = models.CharField(max_length=20, null=True)
+    poster = models.ImageField(null=True)
+    start_location = models.CharField(max_length=100, null=True)
+    destination_location = models.CharField(max_length=100, null=True)
+    destination_date = models.DateField(null=True)
+    trip_policy = models.TextField(null=True)
+    total_days = models.IntegerField(null=True)
+
+class Itenrary(models.Model):
+    trip = models.ForeignKey('Trip', on_delete=models.CASCADE)
+    Day_number = models.CharField(max_length=3)
+    date = models.DateField(null=True)
+    places = models.TextField(null=True)
+    description = models.TextField(null=True)
