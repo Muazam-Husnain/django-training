7bfc0a4629d745a2e723983c0fef3f6926d3133c
MuazamHusnain <muazamhusnain@A006-00309.local>
2020-10-16 17:54:42 +0500

    added another url

--- learn_django/trip_project/urls.py
+++ learn_django/trip_project/urls.py
@@ -4,7 +4,8 @@ from rest_framework import routers
 from . import views
 
 router = routers.DefaultRouter()
-router.register('trips', views.TripView)
+router.register('trips', views.TripsView, 'trips-list')
+router.register(r'trip', views.TripsView, 'trip-details')
 
 urlpatterns = [
     url('api/v1/', include(router.urls)),
