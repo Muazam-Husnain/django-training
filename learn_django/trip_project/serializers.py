591c242e950705036b25eff0ded2fcc686b6b0de
MuazamHusnain <muazamhusnain@A006-00309.local>
2020-10-27 12:38:40 +0500

    made saperate serializers for host and location

--- learn_django/trip_project/serializers.py
+++ learn_django/trip_project/serializers.py
@@ -1,12 +1,26 @@
 from rest_framework import serializers
 from .models import Host, Location, Trip, Itenrary
 
+
+class HostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Host
+        fields = ['host_name', 'host_number']
+
+
+class LocationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Location
+        fields = ['location_name']
+
+
 class TripSerializer(serializers.ModelSerializer):
-    host = serializers.StringRelatedField()
+    host = HostSerializer()
+    start_location = LocationSerializer()
+    destination_location = LocationSerializer()
     created_by = serializers.StringRelatedField()
-    start_location = serializers.StringRelatedField()
-    destination_location = serializers.StringRelatedField()
 
     class Meta:
         model = Trip
         fields = '__all__'
+
